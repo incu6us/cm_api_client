d61052b0638ea5599b116f2bbbe9e44ab418197e
Vyacheslav Pryimak <incu6us@yandex.ru>
2015-04-29 18:46:08 +0300

    small fixes

--- src/com/intropro/main/Main.java
+++ src/com/intropro/main/Main.java
@@ -25,49 +25,7 @@ public class Main {
 		InitApiConnection connection = new InitApiConnection(HOST, USER, PASS);
 		RootResourceV1 apiRootV1 = connection.getApiRootV1();
 
-		/*
-		 * List of clusters
-		 */
-		// ApiClusterList clusters =
-		// apiRootV9.getClustersResource().readClusters(DataView.SUMMARY);
-		// for (ApiCluster cluster : clusters) {
-		// LOG.info(cluster.getName() + " - " + cluster.getVersion());
-		// }
-
-		/*
-		 * Start first cluster
-		 */
-		// ApiCommand cmd =
-		// apiRootV9.getClustersResource().startCommand(clusters.get(0).getName());
-		// while (cmd.isActive()) {
-		// Thread.sleep(100);
-		// cmd = apiRootV9.getCommandsResource().readCommand(cmd.getId());
-		// }
-		//
-		// LOG.info("Cluster start");
-		// LOG.info(cmd.getResultMessage());
-
-		/*
-		 * Get list of services and roles
-		 */
-		// ClustersResourceV9 clustersResource =
-		// apiRootV9.getClustersResource();
-		// for (ApiCluster cluster :
-		// clustersResource.readClusters(DataView.FULL)) {
-		// LOG.info(cluster.getName());
-		// ServicesResourceV2 servicesResource =
-		// clustersResource.getServicesResource(cluster.getName());
-		// for (ApiService service :
-		// servicesResource.readServices(DataView.FULL)) {
-		// LOG.info("\t" + service.getName() + " (service)");
-		// RolesResourceV2 rolesResource =
-		// servicesResource.getRolesResource(service.getName());
-		// for (ApiRole role : rolesResource.readRoles()) {
-		// LOG.info("\t\t" + role.getName() + " (role)");
-		// }
-		// }
-		// }
-
+		
 		/*
 		 * Set config (mapred_queue_names_list)
 		 */
@@ -93,3 +51,4 @@ public class Main {
 		mapReduceService.changeMapredCapacitySchedulerConfiguration(queues);
 	}
 }
+
